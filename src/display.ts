1a0da88b1f532f2514974d2d7f0547e76d9d9034
Julian <mrjrhirst@gmail.com>
2023-01-12 01:45:47 +0000

    Skeleton OOO Framework

---
+++ src/display.ts
@@ -0,0 +1,16 @@
+import{Box, Point} from "geometry";
+import assert from 'node:assert/strict';
+
+class Display {
+    area: Box;
+    buffer : string[][];
+
+    constructor(height: number, width: number) {
+        
+        this.height = height; 
+        this.width = width; 
+        this.origin = {...origin};
+        assert(height > 0 && width > 0 and  )
+    }
+    
+}
